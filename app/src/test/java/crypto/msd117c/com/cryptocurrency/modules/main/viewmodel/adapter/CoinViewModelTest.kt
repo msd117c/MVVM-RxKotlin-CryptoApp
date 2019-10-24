dd688415c9e77a45568587b22c2d6f5c985ecacf
Miguel Sánchez Deleyto <miguel.sanchez@smallworldfs.com>
2019-10-24 08:44:02 +0200

    MVVM-Coroutines: Test passed.

--- app/src/test/java/crypto/msd117c/com/cryptocurrency/modules/main/viewmodel/adapter/CoinViewModelTest.kt
+++ app/src/test/java/crypto/msd117c/com/cryptocurrency/modules/main/viewmodel/adapter/CoinViewModelTest.kt
@@ -28,7 +28,20 @@ class CoinViewModelTest : BaseTest(1) {
     fun test01CheckFormat() {
         Assert.assertEquals("Bitcoin", coinViewModel.getCoinTitle())
         Assert.assertEquals("9.558,56 $", coinViewModel.getCoinUsd())
+        Assert.assertEquals("-0,12%", coinViewModel.getCoinPercentage())
+        Assert.assertEquals("BTC", coinViewModel.getSymbol())
+        Assert.assertEquals("1", coinViewModel.getRank())
+        Assert.assertEquals("171.155.540.318,87  BTC", coinViewModel.getMarketCap())
+        Assert.assertEquals("9.558,56  BTC", coinViewModel.getPriceBtc())
+        Assert.assertEquals("13.728.947.008,28 $", coinViewModel.getVolumeUsd24h())
+        Assert.assertEquals("17.906.012", coinViewModel.getAvailableSupply())
         Assert.assertEquals("17.906.012", coinViewModel.getTotalSupply())
+        Assert.assertEquals("21.000.000", coinViewModel.getMaxSupply())
+        Assert.assertEquals("-0,12% / 1 hour", coinViewModel.getPercentChange1h())
+        Assert.assertEquals("0,33% / 24 hours", coinViewModel.getPercentChange24h())
+        Assert.assertEquals("-8% / 7 days", coinViewModel.getPercentChange7d())
+        Assert.assertEquals("30-2019-08 18:51", coinViewModel.getLastUpdate())
+        Assert.assertEquals(-1, coinViewModel.isPositiveBalance())
 
         testResults[0] = true
     }
